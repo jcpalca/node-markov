3b65fc6336ca9991b381c6ad362209163da88a1b
trevorhudson <trevor.c.hudson@gmail.com>
2022-11-01 17:28:09 -0700

    updated getText to account for null

--- markov.js
+++ markov.js
@@ -31,10 +31,10 @@ class MarkovMachine {
   getChains() {
     let chains = new Map();
 
-    for(let i = 0; i < this.words.length; i++) {
+    for (let i = 0; i < this.words.length; i++) {
       const key = this.words[i];
       const value = this.words[i + 1] || null;
-      if(chains.has(key)) {
+      if (chains.has(key)) {
         chains.get(key).push(value);
       }
       else {
@@ -44,6 +44,7 @@ class MarkovMachine {
 
     // console.log(this.words)
     // console.log(chains)
+    console.log(chains);
     return chains;
   }
 
@@ -59,12 +60,13 @@ class MarkovMachine {
     // - start at the first word in the input text
     // - find a random word from the following-words of that
     // - repeat until reaching the terminal null
-    while(currWord !== null) {
+    while (currWord !== null) {
       const words = this.chains.get(currWord);
       currWord = this.getRandomWord(words);
-      text.push(currWord);
-    }
+      if (currWord !== null) text.push(currWord);
+      else break;
 
+    }
     return text.join(" ");
   }
 
@@ -75,49 +77,12 @@ class MarkovMachine {
   }
 }
 
-const catInHatMachine = new MarkovMachine(`anyone lived in a pretty how town
-with up so floating many bells down
-spring summer autumn winter
-he sang his didn’t he danced his did
+// const catInHatMachine = new MarkovMachine(`The cat is in the hat.
+//   The cat is the cat.
+//   The hat is a cat.`);
 
-Women and men both little and small
-cared for anyone not at all
-they sowed their isn’t they reaped their same
-sun moon stars rain
+// console.log(catInHatMachine.getText());
 
-children guessed but only a few
-and down they forgot as up they grew
-autumn winter spring summer
-that noone loved him more by more
-
-when by now and tree by leaf
-she laughed his joy she cried his grief
-bird by snow and stir by still
-anyone’s any was all to her
-
-someones married their everyones
-laughed their cryings and did their dance
-sleep wake hope and then they
-said their nevers they slept their dream
-
-stars rain sun moon
-and only the snow can begin to explain
-how children are apt to forget to remember
-with up so floating many bells down
-
-one day anyone died i guess
-and noone stooped to kiss his face
-busy folk buried them side by side
-little by little and was by was
-
-all by all and deep by deep
-and more by more they dream their sleep
-noone and anyone earth by april
-wish by spirit and if by yes
-
-Women and men both dong and ding
-summer autumn winter spring
-reaped their sowing and went their came
-sun moon stars rain`);
-
-console.log(catInHatMachine.getText());
+module.exports = {
+  MarkovMachine
+};
